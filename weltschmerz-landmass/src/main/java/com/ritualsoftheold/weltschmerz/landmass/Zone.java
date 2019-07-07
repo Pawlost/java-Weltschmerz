e253a8c0f57d770cd8ee5ba1a45f48099724ac82
Pawlost <mr.p.balusek@gmail.com>
2019-07-07 20:39:54 +0200

    Reworked chunk filling

--- weltschmerz-landmass/src/main/java/com/ritualsoftheold/weltschmerz/landmass/Zone.java
+++ weltschmerz-landmass/src/main/java/com/ritualsoftheold/weltschmerz/landmass/Zone.java
@@ -2,15 +2,16 @@ package com.ritualsoftheold.weltschmerz.landmass;
 
 import com.ritualsoftheold.weltschmerz.landmass.land.Location;
 import com.ritualsoftheold.weltschmerz.noise.generators.WorldNoise;
+import org.apache.commons.collections4.map.MultiKeyMap;
 
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Zone extends ArrayList<Location> {
     private WorldNoise worldNoise;
     private int dirtID;
     private int grassID;
+
 
     public Zone (WorldNoise worldNoise) {
         this.worldNoise = worldNoise;
@@ -18,32 +19,57 @@ public class Zone extends ArrayList<Location> {
 
     public ByteBuffer getChunk(int posX, int posY, int posZ, int bufferSize) {
         ByteBuffer blockBuffer = ByteBuffer.allocate(bufferSize);
+        byte[] fill = new byte[bufferSize];
+        Arrays.fill(fill, (byte) 1);
+        blockBuffer.put(fill);
 
+        MultiKeyMap<Integer, ByteBuffer> blocks = new MultiKeyMap<>();
+        boolean isDifferent = false;
 
-        for(int x = 0; x < 64; x++){
-            for(int z = 0; z < 64; z++) {
+        for(int z = 0; z < 64; z++) {
+            for (int x = 0; x < 64; x++) {
+                ByteBuffer bb = ByteBuffer.allocate(64);
                 int y = (int) Math.round(worldNoise.getNoise(x + posX * 64, z + posZ * 64));
-
-                if(y/64 > posY/64) {
+                if (y / 64 > posY / 64) {
                     byte[] underArray = new byte[64];
                     Arrays.fill(underArray, (byte) dirtID);
-                    blockBuffer.put(underArray);
-                }else  if(y/64 == posY/64) {
-                    byte[] underArray = new byte[(y - posY) - 1];
+                    bb.put(underArray); 
+                    blocks.put(z, x, bb);
+                }else if(y / 64 == posY / 64){
+                    byte[] underArray = new byte[(y%64)];
                     Arrays.fill(underArray, (byte) dirtID);
-                    blockBuffer.put(underArray);
-                    blockBuffer.put((byte) grassID);
-                    byte[] upperArray = new byte[64-(y - posY)];
+                    bb.put(underArray);
+
+                    byte[] upperArray = new byte[64-(y%64)];
                     Arrays.fill(upperArray, (byte) 1);
-                    blockBuffer.put(upperArray);
-                }else {
-                    byte[] upperArray = new byte[64];
-                    Arrays.fill(upperArray, (byte) 1);
-                    blockBuffer.put(upperArray);
+                    bb.put(upperArray);
+
+                    bb.put(y%64, (byte) grassID);
+
+                    isDifferent = true;
+                    blocks.put(z, x, bb);
                 }
             }
         }
 
+        if(isDifferent) {
+            for (int z = 0; z < 64; z++) {
+                for (int x = 0; x < 64; x++) {
+                    if (blocks.containsKey(z, x)) {
+                        for (int y = 0; y < 64; y++) {
+                            blockBuffer.put(x + (y * 64) + (z * 4096), blocks.get(z, x).get(y));
+                        }
+                    }
+                }
+            }
+        }else if(blocks.size() > 0){
+            blockBuffer.clear();
+            blockBuffer.rewind();
+            Arrays.fill(fill, (byte) dirtID);
+            blockBuffer.put(fill);
+        }
+
+        blocks.clear();
         blockBuffer.rewind();
         return blockBuffer;
     }
